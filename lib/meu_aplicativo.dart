0b8f4a5e50b547c57ba8e9a73f95455b21f1dd70
Uildson S. dos Santos <wilder_shon@hotmail.com>
2022-05-04 14:55:17 -0300

    hello world listView

---
+++ lib/meu_aplicativo.dart
@@ -0,0 +1,19 @@
+import 'package:cripto_moedas/pages/moedas_page.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'MoedasBase',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: const MoedasPage(),
+    );
+  }
+}
