03996fd82faacb864bff3f07c9160004826a7e57
Uildson S. dos Santos <wilder_shon@hotmail.com>
2022-05-06 02:05:43 -0300

    customizando formulário e botão

--- lib/pages/moedas_detalhes_page.dart
+++ lib/pages/moedas_detalhes_page.dart
@@ -14,6 +14,21 @@ class MoedasDetalhesPage extends StatefulWidget {
 
 class _MoedasDetalhesPageState extends State<MoedasDetalhesPage> {
   NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
+  final _form = GlobalKey<FormState>();
+  final _valor = TextEditingController();
+  double quantidade = 0;
+
+  comprar() {
+    if (_form.currentState!.validate()) {
+      // Salvar a compra
+
+      Navigator.pop(context);
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Compra realizada com sucesso!')),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,6 +61,81 @@ class _MoedasDetalhesPageState extends State<MoedasDetalhesPage> {
                     ),
                   ),
                 ],
+              ),
+            ),
+            (quantidade > 0)
+                ? SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: Container(
+                      child: Text(
+                        '$quantidade ${widget.moeda.sigla}',
+                        style: const TextStyle(
+                          fontSize: 20,
+                          color: Colors.teal,
+                        ),
+                      ),
+                      margin: const EdgeInsets.only(bottom: 24),
+                      // padding: EdgeInsets.all(12),
+                      alignment: Alignment.center,
+                      // decoration: BoxDecoration(
+                      //   color: Colors.teal.withOpacity(0.05),
+                      // ),
+                    ),
+                  )
+                : Container(
+                    margin: EdgeInsets.only(bottom: 24),
+                  ),
+            Form(
+              key: _form,
+              child: TextFormField(
+                controller: _valor,
+                style: const TextStyle(fontSize: 22),
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Valor',
+                  prefixIcon: Icon(Icons.monetization_on_outlined),
+                  suffix: Text(
+                    'reais',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                ),
+                keyboardType: TextInputType.number,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Informe o valor da compra';
+                  } else if (double.parse(value) < 50) {
+                    return 'Compra mínima é R\$ 50,00';
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  setState(() {
+                    quantidade = (value.isEmpty)
+                        ? 0
+                        : double.parse(value) / widget.moeda.preco;
+                  });
+                },
+              ),
+            ),
+            Container(
+              alignment: Alignment.bottomCenter,
+              margin: EdgeInsets.only(top: 24),
+              child: ElevatedButton(
+                onPressed: comprar,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(Icons.check),
+                    Padding(
+                      padding: EdgeInsets.all(16),
+                      child: Text(
+                        'Comprar',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
