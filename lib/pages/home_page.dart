3838287936e5e191ec3313b397a1c2faa013d93b
Uildson S. dos Santos <wilder_shon@hotmail.com>
2022-05-06 02:29:16 -0300

    Navegação com PageView

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:cripto_moedas/pages/favoritas_page.dart';
 import 'package:cripto_moedas/pages/moedas_page.dart';
 import 'package:flutter/material.dart';
@@ -20,16 +19,34 @@ class _HomePageState extends State<HomePage> {
     pageController = PageController(initialPage: paginaAtual);
   }
 
+  setPaginaAtual(pagina) {
+    setState(() {
+      paginaAtual = pagina;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
         controller: pageController,
-        children: [
-          MoedasPage(),
-          FavoritasPage()
-
+        children: [MoedasPage(), FavoritasPage()],
+        onPageChanged: setPaginaAtual,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: paginaAtual,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Todas'),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favoritas'),
         ],
+        onTap: (pagina) {
+          pageController.animateToPage(
+            pagina,
+            duration: const Duration(milliseconds: 400),
+            curve: Curves.ease,
+          );
+        },
+        // backgroundColor: Colors.grey[100],
       ),
     );
   }
