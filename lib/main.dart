d35bad34ef57e0ea7c5633cb1614381003ea90ef
Uildson S. dos Santos <wilder_shon@hotmail.com>
2022-05-07 14:48:27 -0300

    salvando a preferência de localização do usuário com 'Hive'

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:cripto_moedas/configs/app_settings.dart';
+import 'package:cripto_moedas/configs/hive_config.dart';
 import 'package:cripto_moedas/meu_aplicativo.dart';
 import 'package:cripto_moedas/repositories/favoritas_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await HiveConfig.start();
   runApp(
     MultiProvider(
       providers: [
